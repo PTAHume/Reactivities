01c88cabed9034c03175085e9457795983e08740
Patrick Hume <patricktahume@outlook.com>
2023-07-31 13:18:39 +0100

    init

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,24 @@
+
+//using Domain;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.EntityFrameworkCore;
+//using Persistence;
+//
+//namespace API.Controllers
+//{
+//	public class ActivitiesController : BaseApiController
+//	{
+//		private readonly DataContext _context;
+//
+//		public ActivitiesController(DataContext context) => _context = context;
+//
+//		[HttpGet]
+//		public async Task<ActionResult<IReadOnlyList<Activity>>>
+//			GetActivities() => await _context.Activities.ToListAsync();
+//
+//		[HttpGet("{id}")]
+//		public async Task<ActionResult<Activity>> 
+//			Get(Guid id) => await _context.Activities.FindAsync( id);
+//	}
+//}
+//
