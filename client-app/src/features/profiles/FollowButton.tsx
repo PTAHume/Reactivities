b7d511f11c5e794c9291200f1ceb254cfca46c84
Patrick Hume <patricktahume@outlook.com>
2023-09-29 19:21:42 +0100

    fix following loading

--- client-app/src/features/profiles/FollowButton.tsx
+++ client-app/src/features/profiles/FollowButton.tsx
@@ -2,38 +2,45 @@ import { observer } from "mobx-react-lite";
 import { Profile } from "../../app/modules/profile";
 import { Reveal, Button } from "semantic-ui-react";
 import { useStore } from "../../app/api/stores/store";
-import { SyntheticEvent } from "react";
+import { SyntheticEvent, useState } from "react";
 
 export const FollowButton = observer(({ profile }: { profile: Profile }) => {
   const { profileStore, userStore } = useStore();
   const { updateFollowing, loading } = profileStore;
+  const [target, setTarget] = useState('');
 
   if (userStore.user?.userName == profile.userName) return null;
 
-  const handleButton = (e: SyntheticEvent, username: string) => {
+  const handleButton = (
+    e: SyntheticEvent,
+    username: string,
+  ) => {
     e.preventDefault();
-    updateFollowing(username, !profile.following);
+    setTarget(username);
+    updateFollowing(username, !profile.following).then(() => { setTarget(''); })
   };
 
   return (
-    <Reveal animated="move" disabled={loading}>
+    <Reveal animated="move">
       <Reveal.Content visible>
         <Button
           fluid
           color="teal"
-          content={profile.following ? "Following" : "Not Following"}
-          loading={loading}
-          disabled={loading}
+          name={profile.userName}
+          content={profile.following ? 'Following' : 'Not Following'}
+          loading={target === profile.userName && loading}
+          disabled={target === profile.userName && loading}
         />
       </Reveal.Content>
       <Reveal.Content hidden>
         <Button
           fluid
           basic
-          color={profile.following ? "red" : "green"}
-          content={profile.following ? "Un-follow" : "Follow"}
-          loading={loading}
-          disabled={loading}
+          name={profile.userName}
+          color={profile.following ? 'red' : 'green'}
+          content={profile.following ? 'Un-follow' : 'Follow'}
+          loading={target === profile.userName && loading}
+          disabled={target === profile.userName && loading}
           onClick={(e) => {
             handleButton(e, profile.userName);
           }}
