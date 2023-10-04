d9b770d4bd068d6c78d023496971462b0ee81058
Patrick Hume <patricktahume@outlook.com>
2023-10-04 18:31:20 +0100

    fix utc time error and add docker compose

--- Application/Activities/ActivityParams.cs
+++ Application/Activities/ActivityParams.cs
@@ -4,5 +4,5 @@ public class ActivityParams : PagingPrams
 {
     public bool IsGoing { get; set; }
     public bool IsHost { get; set; }
-    public DateTime StartDate { get; set; } = DateTime.Now;
+    public DateTime StartDate { get; set; } = DateTime.UtcNow;
 }
